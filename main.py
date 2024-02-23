90d6179d4d78cf6af94c5c65731047744badaaab
fareed <29508583+meeeeee@users.noreply.github.com>
2024-02-23 17:00:42 -0500

    rewrote intervention sampling and dag parameter generation functions and sort of checked that they work correctly

--- main.py
+++ main.py
@@ -3,16 +3,17 @@ from model import Model
 import numpy as np
 
 n = 2
-adj = erdag(n, 0.0) # increase for edges
-scm = dagparam(adj)
+adj = erdag(n, 0.7) # increase for edges
+scm = dagparam(adj) # parameters are edge weights with vertex variances on the diagonal (we assume Bii = 0)
 data = sample(scm)
 intrv = np.zeros((n))
-#intrv[-1] = 1
-i_data = sample_intrv(scm, intrv)
+intrv[0] = 2
+i_data = sample_intrv(scm, intrv) # sampling & intervention stuff seems to work correctly
 
 print("Adjacency matrix:", adj)
 print("SCM parameters:\n", scm)
-print("Observational sample:", data)
+print("Observational sample:", i_data)
+"""
 #print("Interventional sample:", i_data, intrv)
 
 initial_nu, initial_T_inv = np.random.standard_normal(n), np.random.standard_normal((n,n)) # initial guess for nu & T_inv
@@ -26,6 +27,7 @@ for _ in range(1000): # it's possible that even with the correct D, recovering T
 T_inv = model.T_inv
 
 print(T_inv, scm)
+"""
 
 # NOTE: the assumption that D can be obtained from the diagonal is incorrect (and is only apparently true for the case where there are no edges)
 # NOTE: I assume for now that the bug is caused by bad code and not by a mistake in my math
